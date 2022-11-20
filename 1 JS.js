5dcd72050a6177418aeefd0a5eca73e7b8277e69
ZU4ARA45 <79358264+ZU4ARA45@users.noreply.github.com>
2022-11-20 10:42:54 -0500

    Add files via upload

---
+++ 1 JS.js
@@ -0,0 +1,66 @@
+/*let a;
+a = 4;
+
+let b = 9;
+
+console.log(a);
+
+
+// const num = 5;
+// num = 9;
+// console.log(num)
+
+var s = 3;
+s = 8;
+console.log(s);*/
+
+
+
+// До 2015 года был ES5 (Ecma Script 5) вообще это типо спецификация (это документ,
+// в котором стороны перечисляют и подробно описывают поставляемые товары,
+// в частности, их наименование, количество, сорта, марки и тому подобное) JS и в это время была var это 
+// глобальная переменная
+
+
+// Потом в этом же 2015 году появиля ES6 и оно было значительно лучше чем ES5 и здесь появились let {} и constant
+// let - если внутри фигурных скобок то весь код за пределами фигурных кобок этот код не видит
+// const - нельзя изменять тобишь если записали например что X равен 4 то его изменит там например на 6 уже не получится
+
+// Типы данных: 
+//  Число (Number)
+// 2, 34, -6, 8.09...
+
+/* Операторы: + - * / остаток от деления (%) SSи возведение в степень (**)
+let num1 = 4;
+let num2 = 6;
+let sum = num1 + num2;
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num1 - num2);
+console.log(num1 ** num2);
+console.log(num2 % num1);*/
+
+/* Строка (string)
+let name = 'Marat';
+let lastname = 'Жук';
+console.log(name);
+console.log(lastname);
+console.log(name, lastname);
+console.log(name + lastname); // Склеивание строк или КОНКТЕНАЦИЯ
+console.log(name + num2);
+console.log('Имя: ' + name + ', '  + 'фамилия: ' + lastname);*/
+
+/*task 2
+
+let i = prompt('Enter your name');
+let d = alert('Hello, ' + i);*/
+
+
+
+/*task 3
+
+let f = prompt('Enter any number');
+let j = prompt('Enter the degree of this number');
+console.log(f ** j);*/
+
+
